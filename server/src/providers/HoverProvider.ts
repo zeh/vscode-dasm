52eda009b497b0d9326086e1b66d374359ee265b
Zeh Fernando <zeh@zehfernando.com>
2017-01-22 16:08:12 -0500

    Hovers now show definitions for instructions and pseudo-ops
    Using the language definition file, we can now easily match hover matches with lists of instructions or pseudo-ops. More items will be matched in the future.

--- server/src/providers/HoverProvider.ts
+++ server/src/providers/HoverProvider.ts
@@ -4,13 +4,10 @@ import {
 	TextDocumentPositionParams,
 } from "vscode-languageserver";
 
-import { IAssemblerResult, ISymbol } from './Assembler';
+import LanguageDefinition from "../definitions/LanguageDefinition";
+import { IAssemblerResult, ISymbol } from "./Assembler";
 
 export default class HoverProvider {
-
-	constructor() {
-
-	}
 
 	/**
 	 * Returns hover information
@@ -31,17 +28,25 @@ export default class HoverProvider {
 					const targetRegex = new RegExp("^.{0," + character + "}\\b([\\w.]*)\\b.*$");
 					const targetMatch = sourceLine.match(targetRegex);
 					if (targetMatch && targetMatch[1]) {
+						// Will search for valid hover strings based on the target
 						const targetName = targetMatch[1];
-						if (results.symbols) {
-							let symbolOrLabel = results.symbols.find((symbol) => symbol.name === targetName);
-							if (symbolOrLabel) {
-								const symbolType = symbolOrLabel.isConstant ? "Constant" : "Label";
-								const symbolValue = symbolOrLabel.value;
-								return {
-									contents: `(${symbolType}) ${targetName} = ${symbolValue} | %${symbolValue.toString(2)} | 0${symbolValue.toString(8)} | $${symbolValue.toString(16)}`,
-									//range:
-								};
-							}
+						let contents:string[]|undefined;
+						let range:{start:number, end:number}|undefined;
+
+						// Check if the target is an instruction
+						if (!contents) contents = this.getInstructionHover(targetName);
+
+						// Check if the target is a pseudo-op
+						if (!contents) contents = this.getPseudoOpsHover(targetName);
+
+						// Check if the target is a symbol or label
+						if (!contents) contents = this.getSymbolOrLabelHover(results, targetName);
+
+						if (contents) {
+							return {
+								contents,
+								//range,
+							};
 						}
 					}
 				}
@@ -49,5 +54,56 @@ export default class HoverProvider {
 		}
 
 		return new ResponseError<void>(0, "Cancelled");
+	}
+
+	private getInstructionHover(target:string):string[]|undefined {
+		const instructionMatch = LanguageDefinition.Instructions.find((instruction) => {
+			return instruction.name.toLowerCase() === target.toLowerCase()
+		});
+		if (instructionMatch) {
+			return [
+				`(instruction) \`${instructionMatch.name}\`: ${instructionMatch.description}`,
+			];
+		}
+	}
+
+	private getPseudoOpsHover(target:string):string[]|undefined {
+		const pseudoOpMatch = LanguageDefinition.PseudoOps.find((pseudoOp) => {
+			return pseudoOp.name.toLowerCase() === target.toLowerCase() ||
+				pseudoOp.otherNames.some((otherName) => otherName.toLowerCase() === target.toLowerCase());
+		});
+		if (pseudoOpMatch) {
+			return [
+				`(pseudo-op) \`${pseudoOpMatch.name}\`: ${pseudoOpMatch.description}`,
+			];
+		}
+	}
+
+	private getSymbolOrLabelHover(results:IAssemblerResult, target:string):string[]|undefined {
+		if (results.symbols) {
+			let symbolOrLabel = results.symbols.find((symbol) => symbol.name === target);
+			if (symbolOrLabel) {
+				if (symbolOrLabel.isConstant) {
+					// Symbol
+					return [
+						`(symbol) \`${symbolOrLabel.name}\``,
+						this.getFormattedValue(symbolOrLabel.value),
+					];
+				} else {
+					// Label
+					return [
+						`(label) \`${symbolOrLabel.name}\``,
+						this.getFormattedValue(symbolOrLabel.value),
+					];
+				}
+			}
+		}
+	}
+
+	private getFormattedValue(value:number):string {
+		return "* Decimal: `" + value + "`\n\n" +
+			"* Binary: `%" + value.toString(2) + "`\n\n" +
+			"* Octal: `0" + value.toString(8) + "`\n\n" +
+			"* Hexa: `$" + value.toString(16) + "`\n\n";
 	}
 }
