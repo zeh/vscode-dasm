83c622b75eba783a38412c13f59602b326acd62a
Zeh Fernando <zeh@zehfernando.com>
2017-01-29 15:04:04 -0500

    Added a new abstract class and interface to be used by all feature providers

---
+++ server/src/providers/Provider.ts
@@ -0,0 +1,35 @@
+import { IConnection } from "vscode-languageserver";
+
+import { IAssemblerResult } from "./Assembler";
+
+export interface IPostCompilationProvider {
+	process(uri:string, sourceLines:string[], results:IAssemblerResult):void;
+}
+
+export type SourceProvider = () => string[]|undefined;
+export type ResultsProvider = () => IAssemblerResult|undefined;
+
+export interface IProjectInfoProvider {
+	getSource:() => string[]|undefined;
+	getResults:() => IAssemblerResult|undefined;
+	getUriForProjectFile:(localUri:string) => string|undefined;
+	getSourceForProjectFile:(uri:string) => string[]|undefined;
+}
+
+export class Provider {
+	private _connection:IConnection;
+	private _projectInfoProvider:IProjectInfoProvider;
+
+	constructor(connection:IConnection, projectInfoProvider:IProjectInfoProvider) {
+		this._connection = connection;
+		this._projectInfoProvider = projectInfoProvider;
+	}
+
+	public getConnection():IConnection {
+		return this._connection;
+	}
+
+	public getProjectInfo():IProjectInfoProvider {
+		return this._projectInfoProvider;
+	}
+}
