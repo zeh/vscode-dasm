a5a38c76d73da42dbdb5a205caa09a32fe285d5a
Zeh Fernando <zeh@zehfernando.com>
2017-07-23 16:44:11 -0400

    Client: removed "Open to the side" command
    We're not going to be using this anymore; we'll use the debug actions instead.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -67,70 +67,10 @@ export function activate(context:ExtensionContext) {
 	context.subscriptions.push(provider);
 	workspace.registerTextDocumentContentProvider("javatari-preview", provider);
 
-	// We need to add all objects to the list of subscriptions
-	// Once the extension is deactivated, the references cease to exist
-	// And they are garbage-collected
-	const disposableCommand = commands.registerCommand("vscode-dasm.openToTheSide", () => {
-		openToTheSide(context);
-	});
-	context.subscriptions.push(disposableCommand);
-
 	console.log("vscode-dasm is now active.");
 }
 
 // Called when the extension is deactivated
 export function deactivate() {
 	console.log("vscode-dasm is now inactive.");
-}
-
-function openToTheSide(context:ExtensionContext) {
-	openExampleGame(context)
-		.then((result) => {
-			console.log("Document opened!");
-			const b = compileGame(context);
-
-		}, window.showErrorMessage);
-
-	createPreviewTab(context)
-		.then((result) => {
-			console.log("Editor shown!");
-			// this.enterExampleGame();
-		}, window.showErrorMessage);
-
-	// TODO:
-	// * Compile the code from the active tab
-	// * Set the new tab with the compile code
-	// * Watch for changes in the previously focused tab
-	// * Update the new tab when changes are detected
-
-	// Future:
-	// * Allow debugging?
-	// * Syntax highlighting and autocompletion?
-	// * Linting and automatic compilation?
-	// * Side panel with compiled byte code?
-	// * Code size on status bar?
-}
-
-function compileGame(context:ExtensionContext) {
-	// Compiles the current tab
-	if (window.activeTextEditor) {
-		const src = window.activeTextEditor.document.getText();
-		// const src = fs.readFileSync(context.asAbsolutePath("assets/_test.asm"), "utf8");
-		const assembler = new RomAssembler(src);
-		console.log("[compile] Done compiling");
-	}
-}
-
-function createPreviewTab(context:ExtensionContext) {
-	const tabPath = Uri.parse("javatari-preview://preview/filename");
-	return commands.executeCommand("vscode.previewHtml", tabPath, ViewColumn.Three);
-}
-
-function openExampleGame(context:ExtensionContext) {
-	// Open a game source in the currently active tab
-	// TODO: this is temporary, for testing purposes only
-	const docPath = Uri.file(context.asAbsolutePath("assets/_test.asm"));
-	return workspace.openTextDocument(docPath).then((doc) => {
-		return window.showTextDocument(doc, ViewColumn.One);
-	});
 }
