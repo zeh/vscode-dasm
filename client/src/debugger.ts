763d1e8259ee1463ce4c731b36eb057614b4e996
Zeh Fernando <zeh@work.co>
2017-08-09 18:21:52 -0400

    Debugger: cleaned up and expanded
    The initial mock debugger has been revamped. Things are commented out and we have a cleaner (feature-wise) debugger implementation to work with.

--- client/src/debugger.ts
+++ client/src/debugger.ts
@@ -28,60 +28,39 @@ import TabServer from "./network/TabServer";
  * This interface should always match the schema found in the mock-debug extension manifest.
  */
 export interface ILaunchRequestArguments extends DebugProtocol.LaunchRequestArguments {
-	program: string;		// An absolute path to the program to debug.
-	stopOnEntry?: boolean;	// Automatically stop target after launch. If not specified, target does not stop
+	program: string;		// An absolute path to the program to debug
+	stopOnEntry?: boolean;	// Automatically stop target after launch; if not specified, target does not stop
 	trace?: boolean;		// Enable logging the Debug Adapter Protocol
 }
 
 class DasmDebugSession extends LoggingDebugSession {
 
-	// we don't support multiple threads, so we can use a hardcoded ID for the default thread
-	private static THREAD_ID = 1;
+	private static readonly THREAD_ID = 1;					// Hardcoded thread id because there's only one thread
 
-	private _server: TabServer<DasmTabProtocol.IMessage>; // Server that communicates with the player tab
+	private _server: TabServer<DasmTabProtocol.IMessage>;	// Server that communicates with the player tab
 
-	// since we want to send breakpoint events, we will assign an id to every event
-	// so that the frontend can match events with breakpoints.
-	private _breakpointId = 1000;
-
-	// This is the next line that will be 'executed'
-	private __currentLine = 0;
-	private get _currentLine(): number {
-		return this.__currentLine;
-	}
-	private set _currentLine(line:number) {
-		this.__currentLine = line;
-		this.log("line", line);
-	}
-
-	// the initial (and one and only) file we are 'debugging'
+	private _breakpointIndex: number = 1000;
+	private _currentAddress: number = 0;
 	private _sourceFile: string;
 
-	// the contents (= lines) of the one and only file
-	private _sourceLines = new Array<string>();
-
-	// maps from sourceFile to array of Breakpoints
 	private _breakPoints = new Map<string, DebugProtocol.Breakpoint[]>();
-
 	private _variableHandles = new Handles<string>();
 
-	/**
-	 * Creates a new debug adapter that is used for one debug session.
-	 * We configure the default implementation of a debug adapter here.
-	 */
 	public constructor() {
-		super("mock-debug.txt");
+		super("dasm-debugger-log.txt");
 
 		// Basic options
 		this.setDebuggerLinesStartAt1(false);
 		this.setDebuggerColumnsStartAt1(false);
 
+		// Create the server so we can talk to the player
 		this._server = new TabServer<DasmTabProtocol.IMessage>(DasmConstants.PLAYER_COMMUNICATION_PORT);
 		this._server.onMessage.add((message) => {
 			console.log("[DEBUGGER] Received server message of type ", message.kind, message);
 		});
 		this._server.onClientConnect.add((id) => {
 			console.log("[DEBUGGER] Connected to client", id);
+			// TODO: also update running state
 		});
 		this._server.onClientDisconnect.add((id) => {
 			console.log("[DEBUGGER] Disconnected from client", id);
@@ -89,56 +68,84 @@ class DasmDebugSession extends LoggingDebugSession {
 	}
 
 	/**
-	 * The 'initialize' request is the first request called by the frontend
-	 * to interrogate the features the debug adapter provides.
+	 * Request called by the frontent to interrogate the features the debug adapter provides.
 	 */
-	protected initializeRequest(response:DebugProtocol.InitializeResponse, args:DebugProtocol.InitializeRequestArguments): void {
+	protected initializeRequest(response: DebugProtocol.InitializeResponse, args: DebugProtocol.InitializeRequestArguments): void {
+		console.log("[DEBUGGER] Initialized with args =", args);
 
-		console.log("initialize!", args);
-
-		// since this debug adapter can accept configuration requests like 'setBreakpoint' at any time,
-		// we request them early by sending an 'initializeRequest' to the frontend.
-		// The frontend will end the configuration sequence by calling 'configurationDone' request.
+		// Asks the frontent to initialize so it can send configuration requests like 'setBreakpoint' at any time;
+		// the frontend will end the configuration sequence by calling 'configurationDone' request.
 		this.sendEvent(new InitializedEvent());
 
 		response.body = response.body || {};
 
-		// This debug adapter implements the configurationDoneRequest.
+		// Implements the configurationDoneRequest
 		response.body.supportsConfigurationDoneRequest = true;
 
-		// make VS Code to use 'evaluate' when hovering over source
-		response.body.supportsEvaluateForHovers = true;
+		// Allows restarting
+		response.body.supportsRestartRequest = true;
 
-		// make VS Code to show a 'step back' button
-		response.body.supportsStepBack = true;
+		// Side effect-free evaluate request for data hovers
+		response.body.supportsEvaluateForHovers = false;
+
+		// Stepping back via the stepBack and reverseContinue requests
+		response.body.supportsStepBack = false;
+
+		/*
+		All features:
+        supportsFunctionBreakpoints?: boolean;
+        supportsConditionalBreakpoints?: boolean;
+        supportsHitConditionalBreakpoints?: boolean;
+        exceptionBreakpointFilters?: ExceptionBreakpointsFilter[];
+        supportsSetVariable?: boolean;
+        supportsRestartFrame?: boolean;
+        supportsGotoTargetsRequest?: boolean;
+        supportsStepInTargetsRequest?: boolean;
+        supportsCompletionsRequest?: boolean;
+        supportsModulesRequest?: boolean;
+        additionalModuleColumns?: ColumnDescriptor[];
+        supportedChecksumAlgorithms?: ChecksumAlgorithm[];
+        supportsExceptionOptions?: boolean;
+        supportsValueFormattingOptions?: boolean;
+        supportsExceptionInfoRequest?: boolean;
+        supportTerminateDebuggee?: boolean;
+		supportsDelayedStackTraceLoading?: boolean;
+		*/
 
 		this.sendResponse(response);
 	}
 
-	protected launchRequest(response:DebugProtocol.LaunchResponse, args:ILaunchRequestArguments): void {
+	protected configurationDoneRequest(response: DebugProtocol.ConfigurationDoneResponse, args: DebugProtocol.ConfigurationDoneArguments) {
+		console.log("[DEBUGGER] Configuration done", args);
+		this.sendResponse(response);
+	}
 
-		// make sure to 'Stop' the buffered logging if 'trace' is not set
+	protected launchRequest(response: DebugProtocol.LaunchResponse, args: ILaunchRequestArguments): void {
+		// Initialize logging
 		logger.setup(args.trace ? Logger.LogLevel.Verbose : Logger.LogLevel.Stop, false);
 
 		console.log("[DEBUGGER] Launched", args);
 
 		this._sourceFile = args.program;
-		this._sourceLines = readFileSync(this._sourceFile).toString().split("\n");
+		const source = readFileSync(this._sourceFile).toString();
 
 		if (args.stopOnEntry) {
-			this._currentLine = 0;
+			this.currentAddress = 0;
 			this.sendResponse(response);
 
-			// we stop on the first line
+			// Stop at the beginning
 			this.sendEvent(new StoppedEvent("entry", DasmDebugSession.THREAD_ID));
 		} else {
-			// we just start to run until we hit a breakpoint or an exception
+			// Start running until a breakpoint or an exception are hit
 			this.continueRequest(response as DebugProtocol.ContinueResponse, { threadId: DasmDebugSession.THREAD_ID });
 		}
 	}
 
-	protected setBreakPointsRequest(response:DebugProtocol.SetBreakpointsResponse, args:DebugProtocol.SetBreakpointsArguments): void {
+	protected setBreakPointsRequest(response: DebugProtocol.SetBreakpointsResponse, args: DebugProtocol.SetBreakpointsArguments): void {
+		// TODO: reimplement
+		this.sendResponse(response);
 
+		/*
 		const path = args.source.path as string;
 		const clientLines = args.lines || [];
 
@@ -168,7 +175,7 @@ class DasmDebugSession extends LoggingDebugSession {
 				}
 			}
 			const bp = new Breakpoint(verified, this.convertDebuggerLineToClient(l)) as DebugProtocol.Breakpoint;
-			bp.id = this._breakpointId++;
+			bp.id = this._breakpointIndex++;
 			breakpoints.push(bp);
 		}
 		this._breakPoints.set(path, breakpoints);
@@ -178,25 +185,29 @@ class DasmDebugSession extends LoggingDebugSession {
 			breakpoints,
 		};
 		this.sendResponse(response);
+		*/
 	}
 
-	protected threadsRequest(response:DebugProtocol.ThreadsResponse): void {
-
-		// return the default thread
+	protected threadsRequest(response: DebugProtocol.ThreadsResponse): void {
+		// We only have one thread
 		response.body = {
 			threads: [
-				new Thread(DasmDebugSession.THREAD_ID, "thread 1"),
+				new Thread(DasmDebugSession.THREAD_ID, "Main thread"),
 			],
 		};
 		this.sendResponse(response);
 	}
 
-	/**
-	 * Returns a fake 'stacktrace' where every 'stackframe' is a word from the current line.
-	 */
-	protected stackTraceRequest(response:DebugProtocol.StackTraceResponse, args:DebugProtocol.StackTraceArguments): void {
+	protected stackTraceRequest(response: DebugProtocol.StackTraceResponse, args: DebugProtocol.StackTraceArguments): void {
+		// TODO: implement
+		response.body = {
+			stackFrames: [],
+			totalFrames: 0,
+		};
+		this.sendResponse(response);
 
-		const words = this._sourceLines[this._currentLine].trim().split(/\s+/);
+		/*
+		const words = this._sourceLines[this.currentAddress].trim().split(/\s+/);
 
 		const startFrame = typeof args.startFrame === "number" ? args.startFrame : 0;
 		const maxLevels = typeof args.levels === "number" ? args.levels : words.length - startFrame;
@@ -208,17 +219,24 @@ class DasmDebugSession extends LoggingDebugSession {
 			const name = words[i];	// use a word of the line as the stackframe name
 			frames.push(new StackFrame(i, `${name}(${i})`, new Source(basename(this._sourceFile),
 				this.convertDebuggerPathToClient(this._sourceFile)),
-				this.convertDebuggerLineToClient(this._currentLine), 0));
+				this.convertDebuggerLineToClient(this.currentAddress), 0));
 		}
 		response.body = {
 			stackFrames: frames,
 			totalFrames: words.length,
 		};
 		this.sendResponse(response);
+		*/
 	}
 
-	protected scopesRequest(response:DebugProtocol.ScopesResponse, args:DebugProtocol.ScopesArguments): void {
+	protected scopesRequest(response: DebugProtocol.ScopesResponse, args: DebugProtocol.ScopesArguments): void {
+		// TODO: implement
+		response.body = {
+			scopes: [],
+		};
+		this.sendResponse(response);
 
+		/*
 		const frameReference = args.frameId;
 		const scopes = new Array<Scope>();
 		scopes.push(new Scope("Local", this._variableHandles.create("local_" + frameReference), false));
@@ -229,10 +247,17 @@ class DasmDebugSession extends LoggingDebugSession {
 			scopes,
 		};
 		this.sendResponse(response);
+		*/
 	}
 
-	protected variablesRequest(response:DebugProtocol.VariablesResponse, args:DebugProtocol.VariablesArguments): void {
+	protected variablesRequest(response: DebugProtocol.VariablesResponse, args: DebugProtocol.VariablesArguments): void {
+		// TODO: implement
+		response.body = {
+			variables: [],
+		};
+		this.sendResponse(response);
 
+		/*
 		const variables = new Array<DebugProtocol.Variable>();
 		const id = this._variableHandles.get(args.variablesReference);
 		if (id !== null) {
@@ -266,11 +291,16 @@ class DasmDebugSession extends LoggingDebugSession {
 			variables,
 		};
 		this.sendResponse(response);
+		*/
 	}
 
-	protected continueRequest(response:DebugProtocol.ContinueResponse, args:DebugProtocol.ContinueArguments): void {
+	protected continueRequest(response: DebugProtocol.ContinueResponse, args: DebugProtocol.ContinueArguments): void {
+		// TODO: implement
+		this.sendResponse(response);
+		this.sendEvent(new TerminatedEvent());
 
-		for (let ln = this._currentLine + 1; ln < this._sourceLines.length; ln++) {
+		/*
+		for (let ln = this.currentAddress + 1; ln < this._sourceLines.length; ln++) {
 			if (this.fireEventsForLine(response, ln)) {
 				return;
 			}
@@ -278,24 +308,35 @@ class DasmDebugSession extends LoggingDebugSession {
 		this.sendResponse(response);
 		// no more lines: run to end
 		this.sendEvent(new TerminatedEvent());
+		*/
 	}
 
-	protected reverseContinueRequest(response:DebugProtocol.ReverseContinueResponse, args:DebugProtocol.ReverseContinueArguments): void {
+	protected reverseContinueRequest(response: DebugProtocol.ReverseContinueResponse, args: DebugProtocol.ReverseContinueArguments): void {
+		// TODO: implement
+		this.sendResponse(response);
+		this.currentAddress = 0;
+		this.sendEvent(new StoppedEvent("entry", DasmDebugSession.THREAD_ID));
 
-		for (let ln = this._currentLine - 1; ln >= 0; ln--) {
+		/*
+		for (let ln = this.currentAddress - 1; ln >= 0; ln--) {
 			if (this.fireEventsForLine(response, ln)) {
 				return;
 			}
 		}
 		this.sendResponse(response);
 		// no more lines: stop at first line
-		this._currentLine = 0;
+		this.currentAddress = 0;
 		this.sendEvent(new StoppedEvent("entry", DasmDebugSession.THREAD_ID));
+		*/
 	}
 
-	protected nextRequest(response:DebugProtocol.NextResponse, args:DebugProtocol.NextArguments): void {
+	protected nextRequest(response: DebugProtocol.NextResponse, args: DebugProtocol.NextArguments): void {
+		// TODO: implement
+		this.sendResponse(response);
+		this.sendEvent(new TerminatedEvent());
 
-		for (let ln = this._currentLine + 1; ln < this._sourceLines.length; ln++) {
+		/*
+		for (let ln = this.currentAddress + 1; ln < this._sourceLines.length; ln++) {
 			if (this.fireStepEvent(response, ln)) {
 				return;
 			}
@@ -303,35 +344,66 @@ class DasmDebugSession extends LoggingDebugSession {
 		this.sendResponse(response);
 		// no more lines: run to end
 		this.sendEvent(new TerminatedEvent());
+		*/
 	}
 
-	protected stepBackRequest(response:DebugProtocol.StepBackResponse, args:DebugProtocol.StepBackArguments): void {
+	protected stepBackRequest(response: DebugProtocol.StepBackResponse, args: DebugProtocol.StepBackArguments): void {
+		// TODO: implement
+		this.sendResponse(response);
+		this.currentAddress = 0;
+		this.sendEvent(new StoppedEvent("entry", DasmDebugSession.THREAD_ID));
 
-		for (let ln = this._currentLine - 1; ln >= 0; ln--) {
+		/*
+		for (let ln = this.currentAddress - 1; ln >= 0; ln--) {
 			if (this.fireStepEvent(response, ln)) {
 				return;
 			}
 		}
 		this.sendResponse(response);
 		// no more lines: stop at first line
-		this._currentLine = 0;
+		this.currentAddress = 0;
 		this.sendEvent(new StoppedEvent("entry", DasmDebugSession.THREAD_ID));
+		*/
 	}
 
-	protected evaluateRequest(response:DebugProtocol.EvaluateResponse, args:DebugProtocol.EvaluateArguments): void {
+	protected evaluateRequest(response: DebugProtocol.EvaluateResponse, args: DebugProtocol.EvaluateArguments): void {
+		// TODO: implement
 
+		/*
 		response.body = {
 			result: `evaluate(context: '${args.context}', '${args.expression}')`,
 			variablesReference: 0,
 		};
 		this.sendResponse(response);
+		*/
+	}
+
+	protected pause(response: DebugProtocol.PauseResponse, args: DebugProtocol.PauseArguments): void {
+		// TODO: implement
+		console.log("[DEBUGGER] Pause request", args);
+	}
+
+	protected restartRequest(response: DebugProtocol.RestartResponse, args: DebugProtocol.RestartArguments): void {
+		// TODO: implement
+		console.log("[DEBUGGER] Restart request", args);
 	}
 
 	protected disconnectRequest(response: DebugProtocol.DisconnectResponse, args: DebugProtocol.DisconnectArguments): void {
 		console.log("[DEBUGGER] Disconnecting", args);
 
+		// TODO: close player tab
+
 		this._server.dispose();
 		this.sendResponse(response);
+	}
+
+	private get currentAddress(): number {
+		return this._currentAddress;
+	}
+
+	private set currentAddress(line: number) {
+		this._currentAddress = line;
+		this.log("line", line);
 	}
 
 	// ---- some helpers
@@ -339,28 +411,35 @@ class DasmDebugSession extends LoggingDebugSession {
 	/**
 	 * Fire StoppedEvent if line is not empty.
 	 */
-	private fireStepEvent(response:DebugProtocol.Response, ln:number): boolean {
+	private fireStepEvent(response: DebugProtocol.Response, ln: number): boolean {
+		// TODO: reimplement
+		return false;
 
+		/*
 		if (this._sourceLines[ln].trim().length > 0) {	// non-empty line
-			this._currentLine = ln;
+			this.currentAddress = ln;
 			this.sendResponse(response);
 			this.sendEvent(new StoppedEvent("step", DasmDebugSession.THREAD_ID));
 			return true;
 		}
 		return false;
+		*/
 	}
 
 	/**
 	 * Fire StoppedEvent if line has a breakpoint or the word 'exception' is found.
 	 */
-	private fireEventsForLine(response:DebugProtocol.Response, ln:number): boolean {
+	private fireEventsForLine(response: DebugProtocol.Response, ln: number): boolean {
+		// TODO: reimplement
+		return false;
 
+		/*
 		// find the breakpoints for the current source file
 		const breakpoints = this._breakPoints.get(this._sourceFile);
 		if (breakpoints) {
 			const bps = breakpoints.filter((bp) => bp.line === this.convertDebuggerLineToClient(ln));
 			if (bps.length > 0) {
-				this._currentLine = ln;
+				this.currentAddress = ln;
 
 				// 'continue' request finished
 				this.sendResponse(response);
@@ -380,7 +459,7 @@ class DasmDebugSession extends LoggingDebugSession {
 
 		// if word 'exception' found in source -> throw exception
 		if (this._sourceLines[ln].indexOf("exception") >= 0) {
-			this._currentLine = ln;
+			this.currentAddress = ln;
 			this.sendResponse(response);
 			this.sendEvent(new StoppedEvent("exception", DasmDebugSession.THREAD_ID));
 			this.log("exception in line", ln);
@@ -388,9 +467,13 @@ class DasmDebugSession extends LoggingDebugSession {
 		}
 
 		return false;
+		*/
 	}
 
-	private log(msg:string, line:number) {
+	/**
+	 * Log onto both the console and the log file
+	 */
+	private log(msg: string, line: number) {
 		const e = new OutputEvent(`${msg}: ${line}\n`);
 		(e as DebugProtocol.OutputEvent).body.variablesReference = this._variableHandles.create("args");
 		this.sendEvent(e);	// print current line on debug console
