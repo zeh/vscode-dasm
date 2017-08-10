62c515ca5c1ab0c7e12e4aeb154f6529c8346d93
Zeh Fernando <zeh@work.co>
2017-08-09 20:08:14 -0400

    Server: fix project manager queued process

--- server/src/project/ProjectFiles.ts
+++ server/src/project/ProjectFiles.ts
@@ -261,8 +261,8 @@ export default class ProjectFiles {
 				process,
 			};
 			this._queuedUpdateProcessId = setTimeout(() => {
-				process();
 				this.clearQueuedFileUpdate();
+				process();
 			}, this._debounceTime);
 		} else {
 			// Can execute immediately
@@ -289,7 +289,7 @@ export default class ProjectFiles {
 			const fullUri = path.resolve(path.join(projectRoot, sourceEntryRelativeUri));
 			const localPath = PathUtils.uriToPlatformPath(fullUri);
 			const file = this.get(fullUri);
-			console.log("[PF] [get] At", projectRoot);
+			console.log("[PF] [get] At root", projectRoot);
 			console.log("[PF] [get] ...Looking for", sourceEntryRelativeUri);
 			console.log("[PF] [get] ...Is", fullUri);
 			if (file && file.contents) {
